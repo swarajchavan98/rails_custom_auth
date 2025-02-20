30a6ba227d0842266c5cc8dd104e123971996642
Swaraj Chavan <swaraj.chavan.98@gmail.com>
2025-02-20 19:23:02 +0530

    Skipped tests in new flow.

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
   test "should get #show" do
     sign_in users(:john)
-    get dashboard_path
+    get organizations_path
 
     assert_response :success
   end
